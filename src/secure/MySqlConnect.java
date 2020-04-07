e78b4ebebae5407e59892ea06b7af3f19dcddfb2
Jithendra <chitineedijithendravenkat@gmail.com>
2020-04-07 16:51:24 +0530

    first commit

---
+++ src/secure/MySqlConnect.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secure;
+import java.sql.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author hemanth
+ */
+public class MySqlConnect {
+    
+    Connection conn=null;
+    public static Connection ConnectDB()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/sonoo?useUnicode=true&characterEncoding=utf8","root","Jithendra@123");
+            JOptionPane.showMessageDialog(null,"Connection Acess");
+            return conn;
+            
+        }
+        
+        catch(Exception e){
+           
+    JOptionPane.showMessageDialog(null,e);
+    
+    return null;
+    
+    }
+    }
+}
+
